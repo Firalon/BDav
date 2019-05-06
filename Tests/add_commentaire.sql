009ef6ec9f750f0c4313bcf928362674282b4970
SAID philippe <said@informatique.univ-paris-diderot.fr>
2019-05-06 11:36:52 +0200

    commentaire et news

---
+++ Tests/add_commentaire.sql
@@ -0,0 +1,4 @@
+       
+INSERT INTO commentaire(id_utilisateur, id_projet, date_creation, message)
+       VALUES
+(12,2,'2019-03-02'::timestamp,'Bravo pour ce projet');
---
+++ Tests/add_news.sql
@@ -0,0 +1,3 @@
+INSERT INTO news(id_projet, date_publication, estPublique, contenu)
+       VALUES
+       (4,'2017-11-15'::timestamp,true,'Bonjour voici les nouveautes');
